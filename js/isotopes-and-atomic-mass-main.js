7265d3a0c5497081add69db76a0a77d47774b3af
jbphet <john.blanco@colorado.edu>
2014-10-16 16:48:32 -0600

    Set up the initial project structure.

---
+++ js/isotopes-and-atomic-mass-main.js
@@ -0,0 +1,42 @@
+//  Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Main entry point for the sim.
+ *
+ * @author John Blanco
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var IsotopesAndAtomicMassScreen = require( 'ISOTOPES_AND_ATOMIC_MASS/isotopes-and-atomic-mass/IsotopesAndAtomicMassScreen' );
+  var Sim = require( 'JOIST/Sim' );
+  var SimLauncher = require( 'JOIST/SimLauncher' );
+
+  // strings
+  var simTitle = require( 'string!ISOTOPES_AND_ATOMIC_MASS/isotopes-and-atomic-mass.name' );
+
+  var simOptions = {
+    credits: {
+      //TODO fill in proper credits, all of these fields are optional, see joist.AboutDialog
+      leadDesign: '',
+      softwareDevelopment: '',
+      team: '',
+      qualityAssurance: '',
+      graphicArts: '',
+      thanks: ''
+    }
+  };
+
+  // Appending '?dev' to the URL will enable developer-only features.
+  if ( window.phetcommon.getQueryParameter( 'dev' ) ) {
+    simOptions = _.extend( {
+      // add dev-specific options here
+    }, simOptions );
+  }
+
+  SimLauncher.launch( function() {
+    var sim = new Sim( simTitle, [ new IsotopesAndAtomicMassScreen() ], simOptions );
+    sim.start();
+  } );
+} );
