4e3851715b0306fc1b33e3bb0f734a503f56d73a
Aadish Gupta <aadish@digitalgreen.org>
2016-05-09 12:29:39 -0600

    initial credits, see issue #52, #59

--- js/isotopes-and-atomic-mass-main.js
+++ js/isotopes-and-atomic-mass-main.js
@@ -22,13 +22,12 @@ define( function( require ) {
 
   var simOptions = {
     credits: {
-      //TODO fill in proper credits, all of these fields are optional, see joist.AboutDialog
-      leadDesign: '',
-      softwareDevelopment: '',
-      team: '',
-      qualityAssurance: '',
-      graphicArts: '',
-      thanks: ''
+      leadDesign: 'Kelly Lancaster',
+      softwareDevelopment: 'John Blanco, Jesse Greenberg, Aadish Gupta, Sam Reid, James Smith',
+      team: 'Jack Barbera, Suzanne Brahmia, Sue Doubler, Amy Hanson,\n' +
+            'Loretta Jones, Kelly Lancaster, Trish Loeblein, Emily B. Moore,\n' +
+            'Robert Parson, Ariel Paul, Kathy Perkins',
+      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Ben Roberts'
     },
     tandem: tandem
   };
