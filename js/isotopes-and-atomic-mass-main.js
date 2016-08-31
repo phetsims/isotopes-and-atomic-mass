8b288451deefed229ba63c618aee8e9d678929ac
Chris Malley <cmalley@pixelzoom.com>
2016-08-31 14:46:56 -0600

    delete no-op simOptions extend from main.js, https://github.com/phetsims/simula-rasa/issues/8

--- js/isotopes-and-atomic-mass-main.js
+++ js/isotopes-and-atomic-mass-main.js
@@ -31,13 +31,6 @@ define( function( require ) {
     }
   };
 
-  // Appending '?dev' to the URL will enable developer-only features.
-  if ( phet.chipper.getQueryParameter( 'dev' ) ) {
-    simOptions = _.extend( {
-      // add dev-specific options here
-    }, simOptions );
-  }
-
   SimLauncher.launch( function() {
     var makeIsotopeScreenTandem = tandem.createTandem( 'makeIsotopeScreen' );
     var mixIsotopeScreenTandem = tandem.createTandem( 'mixIsotopeScreen' );
