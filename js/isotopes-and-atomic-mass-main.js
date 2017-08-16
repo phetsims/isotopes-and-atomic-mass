8a9a3ce677baf41d813fead2c7efbcfe89fe2d7c
zepumph <michael.kauzmann@colorado.edu>
2017-08-15 16:39:03 -0800

    remove unnecessary newlines or replace with <br>, https://github.com/phetsims/joist/issues/435

--- js/isotopes-and-atomic-mass-main.js
+++ js/isotopes-and-atomic-mass-main.js
@@ -23,10 +23,9 @@ define( function( require ) {
   var simOptions = {
     credits: {
       leadDesign: 'Amy Hanson, Kelly Lancaster',
-      softwareDevelopment: 'John Blanco, Jesse Greenberg, Aadish Gupta, \n' +
-        'Sam Reid, James Smith',
-      team: 'Jack Barbera, Suzanne Brahmia, Sue Doubler, Loretta Jones, \n' +
-        'Trish Loeblein, Emily B. Moore, Robert Parson, Ariel Paul, Kathy Perkins',
+      softwareDevelopment: 'John Blanco, Jesse Greenberg, Aadish Gupta, Sam Reid, James Smith',
+       team: 'Jack Barbera, Suzanne Brahmia, Sue Doubler, Loretta Jones, Trish Loeblein, Emily B. Moore, Robert Parson, ' +
+            'Ariel Paul, Kathy Perkins',
       qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Ben Roberts'
     }
   };
