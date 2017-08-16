87f95ddf48dc4d1cb775fab88172446091d4717d
Chris Malley <cmalley@pixelzoom.com>
2017-08-16 10:35:33 -0600

    fix credits formatting, #435

--- js/isotopes-and-atomic-mass-main.js
+++ js/isotopes-and-atomic-mass-main.js
@@ -24,7 +24,7 @@ define( function( require ) {
     credits: {
       leadDesign: 'Amy Hanson, Kelly Lancaster',
       softwareDevelopment: 'John Blanco, Jesse Greenberg, Aadish Gupta, Sam Reid, James Smith',
-       team: 'Jack Barbera, Suzanne Brahmia, Sue Doubler, Loretta Jones, Trish Loeblein, Emily B. Moore, Robert Parson, ' +
+      team: 'Jack Barbera, Suzanne Brahmia, Sue Doubler, Loretta Jones, Trish Loeblein, Emily B. Moore, Robert Parson, ' +
             'Ariel Paul, Kathy Perkins',
       qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Ben Roberts'
     }
