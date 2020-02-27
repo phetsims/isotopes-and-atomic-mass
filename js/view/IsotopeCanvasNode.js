af26a94fcb2a80c18fd6005baffa5cbdc02b7e09
samreid <reids@colorado.edu>
2020-02-27 11:46:45 -0700

    ES6 module migration, see https://github.com/phetsims/chipper/issues/875

--- js/view/IsotopeCanvasNode.js
+++ js/view/IsotopeCanvasNode.js
@@ -5,67 +5,63 @@
  *
  * @author Aadish Gupta
  */
-define( require => {
-  'use strict';
 
-  // modules
-  const CanvasNode = require( 'SCENERY/nodes/CanvasNode' );
-  const inherit = require( 'PHET_CORE/inherit' );
-  const shred = require( 'SHRED/shred' );
+import inherit from '../../../phet-core/js/inherit.js';
+import CanvasNode from '../../../scenery/js/nodes/CanvasNode.js';
+import shred from '../shred.js';
+
+/**
+ * A particle layer rendered on canvas
+ * @param {Array< movableAtom >} isotopes that need to be rendered on the canvas
+ * @param {ModelViewTransform2} modelViewTransform to convert between model and view coordinate frames
+ * @param {Object} [options] that can be passed on to the underlying node
+ * @constructor
+ */
+function IsotopeCanvasNode( isotopes, modelViewTransform, options ) {
+  this.isotopes = isotopes;
+  this.modelViewTransform = modelViewTransform;
+  CanvasNode.call( this, options );
+}
+
+shred.register( 'IsotopeCanvasNode', IsotopeCanvasNode );
+
+export default inherit( CanvasNode, IsotopeCanvasNode, {
 
   /**
-   * A particle layer rendered on canvas
-   * @param {Array< movableAtom >} isotopes that need to be rendered on the canvas
-   * @param {ModelViewTransform2} modelViewTransform to convert between model and view coordinate frames
-   * @param {Object} [options] that can be passed on to the underlying node
-   * @constructor
+   * Paints the particles on the canvas node.
+   * @param {CanvasRenderingContext2D} context
    */
-  function IsotopeCanvasNode( isotopes, modelViewTransform, options ) {
+  paintCanvas: function( context ) {
+    let isotope;
+    let i;
+    const numIsotopes = this.isotopes.length;
+    if ( numIsotopes > 0 ) {
+
+      // only calculate the radius once to save time, assumes they are all the same
+      const radius = this.modelViewTransform.modelToViewDeltaX( this.isotopes.get( 0 ).radiusProperty.get() );
+      context.strokeStyle = 'black';
+
+      for ( i = 0; i < this.isotopes.length; i++ ) {
+        isotope = this.isotopes.get( i );
+        const position = isotope.positionProperty.get();
+        const x = this.modelViewTransform.modelToViewX( position.x );
+        const y = this.modelViewTransform.modelToViewY( position.y );
+        context.fillStyle = isotope.color.toCSS();
+        context.beginPath();
+        context.arc( x, y, radius, 0, 2 * Math.PI, true );
+        context.fill();
+        context.stroke();
+      }
+    }
+  },
+
+  setIsotopes: function( isotopes ) {
     this.isotopes = isotopes;
-    this.modelViewTransform = modelViewTransform;
-    CanvasNode.call( this, options );
+    this.invalidatePaint();
+  },
+
+  step: function() {
+    this.invalidatePaint();
   }
 
-  shred.register( 'IsotopeCanvasNode', IsotopeCanvasNode );
-
-  return inherit( CanvasNode, IsotopeCanvasNode, {
-
-    /**
-     * Paints the particles on the canvas node.
-     * @param {CanvasRenderingContext2D} context
-     */
-    paintCanvas: function( context ) {
-      let isotope;
-      let i;
-      const numIsotopes = this.isotopes.length;
-      if ( numIsotopes > 0 ) {
-
-        // only calculate the radius once to save time, assumes they are all the same
-        const radius = this.modelViewTransform.modelToViewDeltaX( this.isotopes.get( 0 ).radiusProperty.get() );
-        context.strokeStyle = 'black';
-
-        for ( i = 0; i < this.isotopes.length; i++ ) {
-          isotope = this.isotopes.get( i );
-          const position = isotope.positionProperty.get();
-          const x = this.modelViewTransform.modelToViewX( position.x );
-          const y = this.modelViewTransform.modelToViewY( position.y );
-          context.fillStyle = isotope.color.toCSS();
-          context.beginPath();
-          context.arc( x, y, radius, 0, 2 * Math.PI, true );
-          context.fill();
-          context.stroke();
-        }
-      }
-    },
-
-    setIsotopes: function( isotopes ) {
-      this.isotopes = isotopes;
-      this.invalidatePaint();
-    },
-
-    step: function() {
-      this.invalidatePaint();
-    }
-
-  } );
 } );
