79a82fac85620dab821361b066403e9796d07ca0
samreid <reids@colorado.edu>
2020-02-27 12:07:56 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/isotopes-and-atomic-mass-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import isotopesAndAtomicMass from './isotopesAndAtomicMass.js';
+
+const isotopesAndAtomicMassStrings = getStringModule( 'ISOTOPES_AND_ATOMIC_MASS' );
+
+isotopesAndAtomicMass.register( 'isotopesAndAtomicMassStrings', isotopesAndAtomicMassStrings );
+
+export default isotopesAndAtomicMassStrings;
