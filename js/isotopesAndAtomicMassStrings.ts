5f9265925e0f55d0365d712008c1d4c04c16e33f
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:29 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/isotopesAndAtomicMassStrings.ts
@@ -0,0 +1,37 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import isotopesAndAtomicMass from './isotopesAndAtomicMass.js';
+
+type StringsType = {
+  'isotopes-and-atomic-mass': {
+    'title': string
+  },
+  'isotopes': string,
+  'mixtures': string,
+  'neutrons': string,
+  'massNumber': string,
+  'atomicMass': string,
+  'symbol': string,
+  'abundanceInNature': string,
+  'myIsotope': string,
+  'thisIsotope': string,
+  'otherIsotopesPattern': string,
+  'amu': string,
+  'myMix': string,
+  'naturesMix': string,
+  'isotopeMixture': string,
+  'percentComposition': string,
+  'averageAtomicMass': string,
+  'trace': string
+};
+
+const isotopesAndAtomicMassStrings = getStringModule( 'ISOTOPES_AND_ATOMIC_MASS' ) as StringsType;
+
+isotopesAndAtomicMass.register( 'isotopesAndAtomicMassStrings', isotopesAndAtomicMassStrings );
+
+export default isotopesAndAtomicMassStrings;
