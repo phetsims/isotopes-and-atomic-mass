88a72a2794b518961ac55f4b67961cbf6479cae3
phet-dev <phetadmin@gmail.com>
2021-11-26 03:18:20 -0700

    update copyright dates from daily grunt work

--- js/make-isotopes/view/IsotopeAtomNode.js
+++ js/make-isotopes/view/IsotopeAtomNode.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2020, University of Colorado Boulder
+// Copyright 2015-2021, University of Colorado Boulder
 
 /**
  * View representation of the atom.  Mostly, this is responsible for displaying and updating the labels, since the atom
