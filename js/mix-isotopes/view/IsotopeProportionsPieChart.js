18d5f8e77fc8fedf09dc8ab5a228d99063d01a4d
zepumph <michael.kauzmann@colorado.edu>
2022-03-04 13:07:37 -0700

    import kite files from imports.ts, https://github.com/phetsims/kite/issues/96

--- js/mix-isotopes/view/IsotopeProportionsPieChart.js
+++ js/mix-isotopes/view/IsotopeProportionsPieChart.js
@@ -10,7 +10,7 @@
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import Utils from '../../../../dot/js/Utils.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
-import Shape from '../../../../kite/js/Shape.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import { Circle } from '../../../../scenery/js/imports.js';
 import { Node } from '../../../../scenery/js/imports.js';
