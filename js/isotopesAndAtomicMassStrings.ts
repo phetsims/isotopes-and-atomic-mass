3ec009569c77f3a6afb3cfa75599402787efa336
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:33 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/isotopesAndAtomicMassStrings.ts
+++ js/isotopesAndAtomicMassStrings.ts
@@ -5,29 +5,48 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import isotopesAndAtomicMass from './isotopesAndAtomicMass.js';
 
 type StringsType = {
   'isotopes-and-atomic-mass': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'isotopes': string;
+  'isotopesProperty': TReadOnlyProperty<string>;
   'mixtures': string;
+  'mixturesProperty': TReadOnlyProperty<string>;
   'neutrons': string;
+  'neutronsProperty': TReadOnlyProperty<string>;
   'massNumber': string;
+  'massNumberProperty': TReadOnlyProperty<string>;
   'atomicMass': string;
+  'atomicMassProperty': TReadOnlyProperty<string>;
   'symbol': string;
+  'symbolProperty': TReadOnlyProperty<string>;
   'abundanceInNature': string;
+  'abundanceInNatureProperty': TReadOnlyProperty<string>;
   'myIsotope': string;
+  'myIsotopeProperty': TReadOnlyProperty<string>;
   'thisIsotope': string;
+  'thisIsotopeProperty': TReadOnlyProperty<string>;
   'otherIsotopesPattern': string;
+  'otherIsotopesPatternProperty': TReadOnlyProperty<string>;
   'amu': string;
+  'amuProperty': TReadOnlyProperty<string>;
   'myMix': string;
+  'myMixProperty': TReadOnlyProperty<string>;
   'naturesMix': string;
+  'naturesMixProperty': TReadOnlyProperty<string>;
   'isotopeMixture': string;
+  'isotopeMixtureProperty': TReadOnlyProperty<string>;
   'percentComposition': string;
+  'percentCompositionProperty': TReadOnlyProperty<string>;
   'averageAtomicMass': string;
+  'averageAtomicMassProperty': TReadOnlyProperty<string>;
   'trace': string;
+  'traceProperty': TReadOnlyProperty<string>;
 };
 
 const isotopesAndAtomicMassStrings = getStringModule( 'ISOTOPES_AND_ATOMIC_MASS' ) as StringsType;
